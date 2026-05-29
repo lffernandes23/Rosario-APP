541454bf3ad992bb49d7111f1cb637b2a8598112
Luiz Fernando <feodpv02993@odontoprev.com.br>
2026-05-29 17:04:28 -0300

    Correção de BUG no vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
+
+import tailwindcss from "@tailwindcss/vite";
+
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
@@ -8,14 +12,11 @@ export default defineConfig({
 
     react(),
 
+    tailwindcss(),
+
     VitePWA({
 
       registerType: "autoUpdate",
-
-      includeAssets: [
-        "favicon.svg",
-        "apple-touch-icon.png"
-      ],
 
       manifest: {
 
@@ -24,30 +25,26 @@ export default defineConfig({
         short_name: "Rosário",
 
         description:
-          "Aplicativo para oração do Santo Rosário",
+          "Aplicativo para oração do terço e rosário.",
 
         theme_color: "#111827",
 
-        background_color: "#000000",
+        background_color: "#111827",
 
         display: "standalone",
-
-        orientation: "portrait",
-
-        scope: "/",
 
         start_url: "/",
 
         icons: [
 
           {
-            src: "/pwa-192x192.png",
+            src: "/icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
 
           {
-            src: "/pwa-512x512.png",
+            src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
